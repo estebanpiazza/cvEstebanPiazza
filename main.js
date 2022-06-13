e50eb38dcfead2427b189a093a35cee9985acd79
Esteban Piazza <estebanpiazza51@gmail.com>
2022-06-13 01:48:00 -0300

    cambios pagina programador

--- main.js
+++ main.js
@@ -6,5 +6,10 @@ function calcularDatos(){
  let edadEnDias = (hoy-nacimiento)/(1000*3600*24)/365
  let edadEntera = parseInt(edadEnDias)
  document.getElementById("edad").innerHTML = edadEntera
- 
+ let ingresoUTI = new Date('2021-10-1')
+ antiguedadUTI=(hoy-ingresoUTI)/(1000*3600*24)/30
+ let antiguedadUTIEntero = parseInt(antiguedadUTI)
+ document.getElementById("antiguedad-uti").innerHTML = antiguedadUTIEntero
+
 }
+
